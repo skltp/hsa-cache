603e0b81934113746ee41d4755dc978573ed1417
KENTOR\martul <mariia.tuliakova@kentor.se>
2018-04-20 11:46:44 +0200

    add .sh file

---
+++ script/hsaUppdatering.sh
@@ -0,0 +1,3 @@
+source <path till environment filen>/environment
+
+<path till groovy> -Dlogback.configurationFile=<path till logback config>/logback.xml -Dhsa.skript.config=<path till skript config>/application.properties -Dgrape.root=<path till groovy dir>/.groovy/   <path till skriptet>/uppdateraHSACache.groovy
