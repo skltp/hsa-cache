a8d66f2f23b7ab80b2eabab135e5d18c360d63f9
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2023-06-08 08:14:21 +0200

    NTP-2336 dependency cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM groovy:3.0.13-jdk8
 
 COPY --chown=groovy:groovy k8s/* /home/groovy/
+COPY --chown=groovy:groovy target/hsa-cache-*.jar /home/groovy/.groovy/lib/
 
 RUN groovy grabit.groovy
--- k8s/grabit.groovy
+++ k8s/grabit.groovy
@@ -10,16 +10,14 @@
  */
 
 @Grapes([
-        @Grab(group = 'org.apache.httpcomponents', module = 'httpclient', version = '4.5.5'),
         @Grab(group = 'com.sun.mail', module = 'javax.mail', version = '1.6.1'),
-        @Grab(group = 'commons-beanutils', module = 'commons-beanutils', version = '1.9.3'),
+        @Grab(group = 'commons-lang', module = 'commons-lang', version = '2.6'),
         @Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.2.3'),
         @Grab(group = 'net.logstash.logback', module = 'logstash-logback-encoder', version='6.4'),
         @Grab(group = 'io.kubernetes', module = 'client-java', version = '18.0.0'),
-        @Grab(group = 'se.skltp.hsa-cache', module = 'hsa-cache', version = '1.0.1'),
         @GrabConfig(systemClassLoader = true)
 ])
 
-import org.apache.http.util.EntityUtils
+import io.kubernetes.client.openapi.ApiClient;
 
 println 'Done grabbing'
